b3f954d4a48532b5af61c608ef4c4f94fc88ab84
albaii22 <korusshow@gmail.com>
2024-05-16 17:37:15 +0200

    feat: creation of the comments entity

---
+++ src/main/java/com/tfg/backend/entities/Comments.java
@@ -0,0 +1,25 @@
+package com.tfg.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+}
