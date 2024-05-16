25e39d0303a07cbab96134c0266ee9fd0c15d8aa
albaii22 <korusshow@gmail.com>
2024-05-16 17:37:52 +0200

    feat: creation of the publications entity

---
+++ src/main/java/com/tfg/backend/entities/Publications.java
@@ -0,0 +1,27 @@
+package com.fct.BackEnd.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "publications")
+public class Publications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+    private int voteCount;
+
+}
