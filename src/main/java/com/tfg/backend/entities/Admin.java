9839fa5bcceb772c685a5f02a7e74a8242139ad7
albaii22 <korusshow@gmail.com>
2024-05-17 21:29:40 +0200

    fix : fixing admin and adding cascade

--- src/main/java/com/tfg/backend/entities/Admin.java
+++ src/main/java/com/tfg/backend/entities/Admin.java
@@ -1,22 +1,36 @@
 package com.tfg.backend.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "admin")
+@Table(name = "admins")
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-    private int privilegeLevel;
+    private String password;
+    private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Publications> publications;
+
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comments> comments;
+
+    @PrePersist
+    protected void onCreate() {
+        this.creationDate = new Date();
+    }
 }
