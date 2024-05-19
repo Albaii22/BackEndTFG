a59112681fad153b6c87988c3dca59d9499a07b9
albaii22 <korusshow@gmail.com>
2024-05-19 22:59:05 +0200

    fix : fixing img updates

--- src/main/java/com/tfg/backend/service/UserService.java
+++ src/main/java/com/tfg/backend/service/UserService.java
@@ -2,8 +2,11 @@ package com.tfg.backend.service;
 
 import com.tfg.backend.entities.User;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     List<User> getAllUsuarios();
@@ -12,4 +15,5 @@ public interface UserService {
     User updateUsuario(Long id, User user);
     void deleteUsuario(Long id);
     Optional<Long> getUsuarioIdByUsername(String username);
+    User uploadProfileImage(Long id, MultipartFile file) throws IOException;
 }
